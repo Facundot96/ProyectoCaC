952980112117d761a54cfc9d8ef2373d32881d32
Facundo <facundotapia.96@gmail.com>
2021-07-28 04:28:33 -0300

    first commit

---
+++ src/main/java/com/Proyecto/demo/ProyectoCaCApplication.java
@@ -0,0 +1,13 @@
+package com.Proyecto.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoCaCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoCaCApplication.class, args);
+	}
+
+}
